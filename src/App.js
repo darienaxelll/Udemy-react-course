0418fdf33abcd634b20ad6956a165bc8957c9241
Darien Salazar <darien.axell.salazar@gmail.com>
2022-11-07 16:21:11 -0600

    Learning about classes in React

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 
+class Human {
+    constructor() {
+        this.gender = 'male';
+    }
+
+    printGender() {
+        console.log(this.gender);
+    }
+}
+
+class Person extends Human {
+    constructor() {
+        super();
+        this.name = 'Max';
+        this.gender = 'female';
+    }
+
+    printMyName() {
+        console.log(this.name);
+    }
+}
+
+const person = new Person();
+person.printMyName();
+person.printGender();
+
 function App() {
   return (
     <div className="App">
--- src/exercises.js
+++
@@ -1,8 +0,0 @@
-const myName = "Darien";
-
-// function printMyName() {
-//     return console.log(myName);
-// }
-
-const printMyName = () => console.log(myName);
-
